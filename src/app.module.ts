c1770e982ec0839c7f43dbb74380a47b3b08cd39
yuanzishun <yuanzishun@kuaishou.com>
2021-12-26 16:37:25 +0800

    初始化env配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
+const ENV = process.env.NODE_ENV;
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ENV ? `.env.${ENV}` : '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
